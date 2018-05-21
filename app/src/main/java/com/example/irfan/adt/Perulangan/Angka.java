6bb670e87cc902458eaf7cc8ad615ba42e0a64c1
rhandy novaruddin <rhandy.nova@gmail.com>
2018-05-21 16:31:53 +0700

    rendy

--- app/src/main/java/com/example/irfan/adt/Perulangan/Angka.java
+++
@@ -1,9 +0,0 @@
-package com.example.irfan.adt.Perulangan;
-
-public class Angka {
-    public static void main(String[] args){
-        for(int i=0;i<=10;i++){
-            System.out.println(i);
-        }
-    }
-}
